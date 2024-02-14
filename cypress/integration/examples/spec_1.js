c9e1f51928890590eddd13e147b446ccb3cb441d
Mrishi712 <itsmerishi712@gmail.com>
2024-02-15 00:43:47 +0530

    Phase1

--- cypress/integration/examples/spec_1.js
+++ cypress/integration/examples/spec_1.js
@@ -1,5 +1,10 @@
-describe('My First Test', () => {
-    it('Visits the Kitchen Sink', () => {
-        cy.visit('https://example.cypress.io')
+describe('Cypress Suite 01', () => {
+    it('Navigate to Home page', () => {
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+
+        cy.get('.search-keyword').type("ca")
+
+        //we can sort out the visible test using :visible 
+        cy.get('.product:visible').should("have.length",4)
     })
 })
