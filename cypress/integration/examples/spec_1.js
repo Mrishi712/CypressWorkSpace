9ec563d9d8df0a709ada0e0d1d35f9661c67c5d8
Mrishi712 <itsmerishi712@gmail.com>
2024-02-18 14:33:39 +0530

    Code!

--- cypress/integration/examples/spec_1.js
+++ cypress/integration/examples/spec_1.js
@@ -4,7 +4,25 @@ describe('Cypress Suite 01', () => {
 
         cy.get('.search-keyword').type("ca")
 
+
         //we can sort out the visible test using :visible 
-        cy.get('.product:visible').should("have.length",4)
+        cy.get('.product:visible').should("have.length", 4)
+        //paraent child chaining
+        cy.get('.products').find('.product').should("have.length", 4)
+        cy.get('.products').find('.product').then(ele => {
+            cy.log("Length", ele.length)
+        })
+
+        cy.get('.products').find('.product').eq(1).contains('ADD TO CART').click()
+
+        cy.get('.products').find('.product').each(($el, index, $list) => {
+            let vegName=$el.find('h4.product-name').text()
+            cy.log("Veg Name",vegName)
+            if(vegName.includes('Cashews')){
+                cy.wrap($el).find('button').click()
+            }
+        })
+        
+        console.log("End");
     })
 })
